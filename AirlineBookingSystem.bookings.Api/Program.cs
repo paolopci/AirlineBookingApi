855076a582b6efb229abef99bf538b30c7a869b6
Paolo Paci <paolopci@yahoo.it>
2025-07-16 17:08:03 +0200

    Booking Repository Infrastructure

--- AirlineBookingSystem.bookings.Api/Program.cs
+++ AirlineBookingSystem.bookings.Api/Program.cs
@@ -1,3 +1,9 @@
+using System.Data;
+using AirlineBookingSystem.Bookings.Core.Repositories;
+using AirlineBookingSystem.Bookings.Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Application Services
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IDbConnection>(sp => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
