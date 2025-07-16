8ffc836665af9adcb5356e0cc54519efb2a73ee3
Paolo Paci <paolopci@yahoo.it>
2025-07-16 15:57:15 +0200

    Create Interface .. repository

---
+++ AirlineBookingSystem.Notifications.Core/Repositories/INotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirlineBookingSystem.Notifications.Core.Entities;
+
+
+namespace AirlineBookingSystem.Notifications.Core.Repositories
+{
+    public interface INotificationRepository
+    {
+        Task LogNotificationAsync(Notification notification);
+    }
+}
---
+++ AirlineBookingSystem.Payments.Core/Repositories/IPaymentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirlineBookingSystem.Payments.Core.Entities;
+
+
+namespace AirlineBookingSystem.Payments.Core.Repositories
+{
+    public interface IPaymentRepository
+    {
+        Task ProcessPaymentAsync(Payment payment);
+        Task RefundPaymentAsync(Guid id);
+
+    }
+}
